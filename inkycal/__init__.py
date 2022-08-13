c3b8e4ee2da0e7aeda446fa013802d7a2c993f22
Boomer Ransom <ransom.b.david@gmail.com>
2022-08-13 12:31:50 -0400

    cleaning up

--- inkycal/__init__.py
+++ inkycal/__init__.py
@@ -1,19 +1,9 @@
 # Display class (for dirving E-Paper displays)
 from inkycal.display import Display
 
-# Default modules
-# import inkycal.modules.inkycal_agenda
-# import inkycal.modules.inkycal_calendar
 import inkycal.modules.inkycal_weather
-
-# import inkycal.modules.inkycal_feeds
-# import inkycal.modules.inkycal_todoist
-import inkycal.modules.inkycal_image
-
 import inkycal.modules.inkycal_jokes
 import inkycal.modules.inkycal_slideshow
-
-# import inkycal.modules.inkycal_server
 import inkycal.modules.inkycal_dogtracker
 import inkycal.modules.inkycal_counter
 import inkycal.modules.inkycal_gcalendar
@@ -21,5 +11,3 @@ import inkycal.modules.inkycal_gcalendar
 # Main file
 from inkycal.main import Inkycal
 from inkycal.game import InkyCalWrapper
-
-# import inkycal.modules.inkycal_stocks
